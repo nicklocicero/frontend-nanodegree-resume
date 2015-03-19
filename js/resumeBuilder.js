d400ce56c8aa7cc70048c848f0482058ea3e4d5c
nicklocicero <nicklocicero@gmail.com>
2015-03-19 11:50:35 -0700

    Setup resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,72 @@
+//var formattedName = HTMLheaderName.replace("%data%", "Nicholas LoCicero");
+//var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+//$("#header").append([formattedName, formattedRole]);
+
+
+var bio = {
+  "name": "Nicholas LoCicero",
+  "role": "Web Developer",
+  "contactInfo": "Twitter @viraldreams",
+  "picURL": "http://s3-2.kiva.org/img/w800/451751.jpg",
+  "welcomeMessage": "Thank you for coming by!",
+  "skills": ["hacking", "web-development", "resourcefulness"]
+};
+
+var education = {
+  "schools": [
+      {
+          "school": "UC Berkeley",
+          "majors": ["Art Practice"],
+          "minor": ["Theater"],
+          "years": "2005 to 2010",
+          "city": "Berkeley, CA",
+          "graduation": "2010"
+      },
+      {
+          "school": "Udacity",
+          "major": "Nanodegree: Front-End Web Dev",
+          "years": "2015",
+          "city": "online",
+          "graduation": "2015"
+      },
+      {
+          "school": "Coursera",
+          "major": "Specialization in Music",
+          "years": "2014 - 2015",
+          "city": "online",
+          "graduation": "2015"
+      }
+  ]
+}
+
+var work = {
+  "Jobs": [
+    {
+      "company": "Zipfian Academy",
+      "role": "Consultant"
+      "description": "Student Career Services",
+      "year": "2014"
+    },
+    {
+      "company": "Hack Reactor",
+      "role": "Consultant"
+      "description": "Student Career Services",
+      "year": "2014"
+    },
+    {
+      "company": "Task Rabbit",
+      "role": "Task Rabbit",
+      "description": "Tasked for Uber, Pinterest, Hipchat, Twilio, and more",
+      "year": "2013 - 2014"
+    }
+  ]
+}
+
+var projects = {
+  "Projects": []
+}
+
+//$("#main").append([bio.currentJob, education["school"]]);
+
+//$("#header").append([bio]);
